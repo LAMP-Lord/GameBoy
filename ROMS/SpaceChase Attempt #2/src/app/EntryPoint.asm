12b5054929f7f2800d0933977ebd981305621f5c
LAMP-Lord <bgreninger@creativecag.com>
2025-03-05 00:52:13 -0600

    font edited

--- ROMS/SpaceChase Attempt #2/src/app/EntryPoint.asm
+++ ROMS/SpaceChase Attempt #2/src/app/EntryPoint.asm
@@ -6,6 +6,8 @@ SECTION "Header", ROM0[$100]
 
     ds $150 - @, 0
 
+SECTION "Entry Point", ROM0
+
 EntryPoint:
     ld a, LCDCF_OFF
     ld [rLCDC], a
@@ -13,10 +15,10 @@ EntryPoint:
     ld hl, song
     call hUGE_init
 
+    ld a, %00_01_10_11
+    ld [rBGP], a
+
     call Text_LoadFont
 
     ld a, LCDCF_ON | LCDCF_BGON | LCDCF_OBJON | LCDCF_OBJ8 | LCDCF_WINON | LCDCF_WIN9C00
     ld [rLCDC], a
-
-Loop:
-    jp Loop
